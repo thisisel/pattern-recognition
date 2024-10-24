71002be7e8e592085a292576813162320332ace4
Elahe <1997.esm@gmail.com>
2024-10-24 17:10:20 +0330

    updated gitignore

--- mini_proj1/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-grabbed_s_sample_pixels = read_samples(sky_pic_paths)
-stacked_s_sample_pixels = stack_all_samples(grabbed_s_sample_pixels)
-
-print(
-    f"total number of pixels gathered from {len(sky_pic_paths)} samples: {stacked_s_sample_pixels.shape[0]}"
-)
